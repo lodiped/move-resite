9d9a6e31ecfff01ec815c8726ab0ef5b23ff3760
lodiped <lodiped@gmail.com>
2024-12-06 13:34:11 -0300

    podcast: episodios, mockup

--- src/routes/podcast/+page.svelte
+++ src/routes/podcast/+page.svelte
@@ -6,6 +6,10 @@
 	import movelogo from '$lib/assets/nej/move.png';
 	import movecorlogo from '$lib/assets/nej/movecor.png';
 	import ztxlogo from '$lib/assets/nej/ztx.png';
+	import angelo from '$lib/assets/nej/thumbs/Angelo.png';
+	import naganos from '$lib/assets/nej/thumbs/Naganos.png';
+	import heroNej from '$lib/assets/nej/hero.png';
+	import headerNej from '$lib/assets/nej/header.png';
 
 	// @ts-ignore
 	import Amazon from 'virtual:icons/cbi/amazon-logo';
@@ -19,9 +23,6 @@
 	import YouTubeMusic from 'virtual:icons/cbi/youtube-music';
 	// @ts-ignore
 	import Spotify from 'virtual:icons/mdi/spotify';
-
-	import heroNej from '$lib/assets/nej/hero.png';
-	import headerNej from '$lib/assets/nej/header.png';
 
 	// ´Go to´ seção com padding especificos
 	/** @param {number} offset */
@@ -66,8 +67,8 @@
 
 <header
 	class={atTop
-		? 'fixed text-nejblack rounded-b-3xl bg-nej py-4 transition-all top-0 left-0 w-full flex justify-center'
-		: 'fixed text-nejblack rounded-b-3xl py-4 bg-nej/50 transition-all backdrop-blur border-b border-black/5 drop-shadow top-0 left-0 w-full flex justify-center'}
+		? 'fixed z-50 text-nejblack rounded-b-3xl bg-nej py-4 transition-all top-0 left-0 w-full flex justify-center'
+		: 'fixed z-50 text-nejblack rounded-b-3xl py-4 bg-nej/50 transition-all backdrop-blur border-b border-black/5 drop-shadow top-0 left-0 w-full flex justify-center'}
 >
 	<div class="w-[1200px] h-12 flex justify-between items-center">
 		<a href="/podcast"><img src={headerNej} alt="" class=" w-48" /></a>
@@ -212,15 +213,59 @@
 		</div>
 	</div>
 
-	<div id="episodios" class="flex flex-col items-center gap-10 w-[1200px]">
+	<div id="episodios" class="flex flex-col items-center gap-20 w-[1200px]">
 		<h2 class="font-cofo text-6xl">Episódios</h2>
-		<div class="flex gap-4">
-			<div class="h-[180px] w-[320px] rounded-lg bg-stone-400"></div>
-			<div class="flex flex-col justify-between">
+		<div class="flex gap-4 justify-between w-full">
+			<img src={angelo} alt="" class="h-[200px] w-[355px] rounded-xl drop-shadow" />
+			<div class="flex flex-col justify-start gap-5 w-[810px] h-[250px]">
+				<h3 class="font-bold text-xl">#43 - Ângelo Max Donaton (Lavô)</h3>
+				<p>
+					Nosso convidado é o Angelo Max Donaton, o empreendedor por trás da Lavô, a maior rede de
+					lavanderias self-service do país, com mais de mil unidades! Angelo largou uma carreira de
+					25 anos como representante comercial para apostar em um modelo de lavanderia que alia
+					praticidade e autonomia, inspirando-se nas experiências de autoatendimento nos Estados
+					Unidos e na dificuldade que ele próprio observou em viagens.
+				</p>
+				<div class="flex gap-2 *:border-nejblack">
+					<div class="text-2xl scale-110"><YouTube /></div>
+					<div class="text-2xl"><Spotify /></div>
+					<div class="text-2xl scale-90"><YouTubeMusic /></div>
+					<div class="text-2xl -translate-y-0.5 scale-105"><Apple /></div>
+				</div>
+			</div>
+		</div>
+		<div class="flex gap-4 justify-between w-full">
+			<img src={naganos} alt="" class="h-[200px] w-[355px] rounded-xl drop-shadow" />
+			<div class="flex flex-col justify-start gap-5 w-[810px] h-[250px]">
+				<h3 class="font-bold text-xl">#42 - Marcos e Lucas Nagano (10 Pastéis)</h3>
+				<p>
+					Conheça a jornada de Marcos e Lucas Nagano, os visionários por trás da rede 10 Pastéis,
+					que transformaram o tradicional pastel em um negócio milionário! Marcos largou uma
+					carreira consolidada para apostar no empreendedorismo, enquanto seu filho, Lucas, agora
+					Diretor de Novos Negócios, está liderando a expansão da marca, que já fatura R$ 65 milhões
+					e planeja dobrar até 2025! Quer saber como eles fizeram o negócio crescer e quais os
+					segredos para inovar em um setor tão competitivo? Não perca esse episódio!
+				</p>
+				<div class="flex gap-2 *:border-nejblack">
+					<div class="text-2xl scale-110"><YouTube /></div>
+					<div class="text-2xl"><Spotify /></div>
+					<div class="text-2xl scale-90"><YouTubeMusic /></div>
+					<div class="text-2xl -translate-y-0.5 scale-105"><Apple /></div>
+				</div>
+			</div>
+		</div>
+		<div class="flex gap-4 justify-between w-full">
+			<div class="h-[200px] w-[355px] rounded-lg bg-stone-400"></div>
+			<div class="flex flex-col justify-start gap-5 w-[810px] h-[250px]">
 				<h3 class="font-bold text-xl">#01 Titulo do Episódio</h3>
 				<p>
-					Descrição do episódio Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Voluptatibus, impedit.
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi tempore praesentium
+					quibusdam perspiciatis. Commodi saepe minima cum fugiat praesentium nemo quasi ipsum
+					nesciunt ad, asperiores eaque voluptas voluptates aut ipsam accusantium sequi alias quos
+					hic itaque. Sit porro suscipit ipsam sequi repellendus enim expedita dicta recusandae
+					veniam, molestias consequatur autem mollitia, officiis corrupti. Facere quo quidem eos
+					vero recusandae cumque? Quo tempore explicabo consequuntur pariatur consequatur quos
+					aliquam architecto qui.
 				</p>
 				<div class="flex gap-2 *:border-nejblack">
 					<div class="w-8 h-8 rounded-lg border"></div>
@@ -230,45 +275,18 @@
 				</div>
 			</div>
 		</div>
-		<div class="flex gap-4">
-			<div class="h-[180px] w-[320px] rounded-lg bg-stone-400"></div>
-			<div class="flex flex-col justify-between">
+		<div class="flex gap-4 justify-between w-full">
+			<div class="h-[200px] w-[355px] rounded-lg bg-stone-400"></div>
+			<div class="flex flex-col justify-start gap-5 w-[810px] h-[250px]">
 				<h3 class="font-bold text-xl">#01 Titulo do Episódio</h3>
 				<p>
-					Descrição do episódio Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Voluptatibus, impedit.
-				</p>
-				<div class="flex gap-2 *:border-nejblack">
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-				</div>
-			</div>
-		</div>
-		<div class="flex gap-4">
-			<div class="h-[180px] w-[320px] rounded-lg bg-stone-400"></div>
-			<div class="flex flex-col justify-between">
-				<h3 class="font-bold text-xl">#01 Titulo do Episódio</h3>
-				<p>
-					Descrição do episódio Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Voluptatibus, impedit.
-				</p>
-				<div class="flex gap-2 *:border-nejblack">
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-					<div class="w-8 h-8 rounded-lg border"></div>
-				</div>
-			</div>
-		</div>
-		<div class="flex gap-4">
-			<div class="h-[180px] w-[320px] rounded-lg bg-stone-400"></div>
-			<div class="flex flex-col justify-between">
-				<h3 class="font-bold text-xl">#01 Titulo do Episódio</h3>
-				<p>
-					Descrição do episódio Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Voluptatibus, impedit.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eveniet velit quos rerum
+					doloribus debitis fugiat ullam voluptatem eum corporis consequuntur culpa odit itaque
+					dignissimos minima voluptas labore non facilis quod quis ipsum architecto aspernatur vero.
+					Deleniti aliquam voluptates magni. Magni consequatur necessitatibus facilis ipsam iure,
+					dignissimos corrupti assumenda quaerat cupiditate numquam qui nobis aspernatur accusamus
+					optio placeat architecto at tempora eos temporibus enim harum ducimus vero. Vitae,
+					explicabo architecto!
 				</p>
 				<div class="flex gap-2 *:border-nejblack">
 					<div class="w-8 h-8 rounded-lg border"></div>
