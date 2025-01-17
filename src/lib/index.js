655c28622948cf1254e0ce157d73dfa4ee1049fd
lodiped <lodiped@gmail.com>
2025-01-17 13:11:15 -0300

    colaboradores novos

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,21 +3,27 @@
 import alinefoto from '$lib/assets/colaboradores/Aline.webp';
 import amandafoto from '$lib/assets/colaboradores/Amanda.webp';
 import andreusfoto from '$lib/assets/colaboradores/Andreus.webp';
+import brandomfoto from '$lib/assets/colaboradores/Brandom.webp';
 import cassiafoto from '$lib/assets/colaboradores/Cassia.webp';
+import crisfoto from '$lib/assets/colaboradores/Cristina.webp';
 import betefoto from '$lib/assets/colaboradores/Bete.webp';
 import ewertonfoto from '$lib/assets/colaboradores/Ewerton.webp';
 import fernandofoto from '$lib/assets/colaboradores/Fernandinho.webp';
+import franfoto from '$lib/assets/colaboradores/Franciely.webp';
 import ketelinfoto from '$lib/assets/colaboradores/Ketelin.webp';
 import larifoto from '$lib/assets/colaboradores/Larissa.webp';
 import lauanyfoto from '$lib/assets/colaboradores/Lauany.webp';
 import lincolnfoto from '$lib/assets/colaboradores/Lincoln.webp';
+import lyncolnfoto from '$lib/assets/colaboradores/Lyncoln.webp';
 import michaelfoto from '$lib/assets/colaboradores/Michael.webp';
-import nunesfoto from '$lib/assets/colaboradores/Jessica.webp';
+import niufoto from '$lib/assets/colaboradores/Niulen.webp';
+import nunesfoto from '$lib/assets/colaboradores/Jéssica.webp';
 import phelipefoto from '$lib/assets/colaboradores/Phelipe.webp';
 import rafaelfoto from '$lib/assets/colaboradores/Rafael.webp';
 import robersonfoto from '$lib/assets/colaboradores/Roberson.webp';
 import tamirisfoto from '$lib/assets/colaboradores/Tamiris.webp';
 import thiagofoto from '$lib/assets/colaboradores/Thiago.webp';
+import thiagolfoto from '$lib/assets/colaboradores/Thiago-L.webp';
 import zeufoto from '$lib/assets/colaboradores/Eliseu.webp';
 import pedrofoto from '$lib/assets/colaboradores/Pedro.webp';
 import andrefoto from '$lib/assets/colaboradores/Andre.webp';
@@ -27,15 +33,20 @@ export const colaboradoresimg = [
 	alinefoto,
 	amandafoto,
 	andreusfoto,
+	brandomfoto,
 	betefoto,
 	cassiafoto,
+	crisfoto,
 	fernandofoto,
+	franfoto,
 	ewertonfoto,
 	ketelinfoto,
 	larifoto,
 	lauanyfoto,
 	lincolnfoto,
+	lyncolnfoto,
 	michaelfoto,
+	niufoto,
 	nunesfoto,
 	pedrofoto,
 	phelipefoto,
@@ -43,6 +54,7 @@ export const colaboradoresimg = [
 	robersonfoto,
 	tamirisfoto,
 	thiagofoto,
+	thiagolfoto,
 	zeufoto,
 	andrefoto,
 	valdifoto
