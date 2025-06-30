4740094c6b6bf13f0c881cab567c6a6864a96893
lodiped <lodiped@gmail.com>
2025-06-30 13:37:49 -0300

    carousel fixed

--- src/lib/components/EmblaColaboradores.svelte
+++ src/lib/components/EmblaColaboradores.svelte
@@ -8,18 +8,14 @@
 	let plugins = [AutoScroll({ startDelay: 0, stopOnInteraction: false, stopOnFocusIn: false })];
 </script>
 
-<div
-	class="flex pt-20 pb-10 justify-center flex-col items-center drop-shadow-[0_0_1rem_rgba(0,0,0,0.75)]"
->
-	<div class="overflow-hidden select-none" use:emblaCarouselSvelte={{ options, plugins }}>
-		<div class="flex">
-			{#each colaboradoresimg as colaborador}
-				<div class="lg:flex-[0_0_330px] flex-[0_0_150px] min-w-0">
-					<div class="w-full drop-shadow-[0_5px_0.5rem_rgba(0,0,0,0.7)]">
-						<img src={colaborador} alt="" />
-					</div>
+<div class="overflow-hidden select-none pb-10" use:emblaCarouselSvelte={{ options, plugins }}>
+	<div class="flex">
+		{#each colaboradoresimg as colaborador}
+			<div class="lg:flex-[0_0_330px] flex-[0_0_150px] min-w-0">
+				<div class="w-full drop-shadow-[0_5px_0.5rem_rgba(0,0,0,0.7)]">
+					<img src={colaborador} alt="" />
 				</div>
-			{/each}
-		</div>
+			</div>
+		{/each}
 	</div>
 </div>
